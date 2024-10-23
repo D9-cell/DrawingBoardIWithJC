924185c8aa791298f76bc7b0ebd4c489f8e654bb
Deepon Das <deeponsubhro@gmail.com>
2024-10-23 11:43:08 +0530

    Perfect Drawing App till now

--- app/src/main/java/com/example/drawingboardiwithjc/DrawingViewScreen.kt
+++ app/src/main/java/com/example/drawingboardiwithjc/DrawingViewScreen.kt
@@ -3,29 +3,12 @@ package com.example.drawingboardiwithjc
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -89,178 +72,146 @@ fun DrawingViewScreen() {
         backgroundColor = color
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .weight(14f)
-            .border(2.dp, color = Color.DarkGray)
-        ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(14f)
+                    .border(2.dp, color = Color.DarkGray)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .background(backgroundColor) // Use the backgroundColor state
+                )
 
-            Box(modifier = Modifier
-                .matchParentSize()
-                .background(backgroundColor) // Use the backgroundColor state
-            )
-
-            AndroidView(
-                modifier = Modifier.fillMaxSize(),
-                factory = { context ->
-                    DrawingView(context, null).apply {
-                        drawingView.value = this
-                        setColor(String.format("#%06X", 0xFFFFFF and selectedColor.toArgb())) // Set initial color
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(),
+                    factory = { context ->
+                        DrawingView(context, null).apply {
+                            drawingView.value = this
+                            setColor(String.format("#%06X", 0xFFFFFF and selectedColor.toArgb())) // Set initial color
+                        }
+                    },
+                    update = { view ->
+                        view.setColor(String.format("#%06X", 0xFFFFFF and selectedColor.toArgb()))
                     }
-                },
-                update = { view ->
-                    view.setColor(String.format("#%06X", 0xFFFFFF and selectedColor.toArgb()))
-                }
+                )
+            }
+        }
+
+        // FloatingActionButton for Brush Size at the bottom-right corner
+        FloatingActionButton(
+            onClick = { showBrushBottomSheet = true }, // Open brush size changer on click
+            modifier = Modifier
+                .align(Alignment.BottomEnd) // Align to the bottom-right
+                .padding(16.dp)
+                .size(54.dp)// Add padding from the edge
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.brush), // Use brush icon
+                contentDescription = "Brush Size",
+                tint = Color.Unspecified
             )
         }
 
-        CustomBottomBar(
-            onBrushSizeChange = { showBrushBottomSheet = true },
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        )
-    }
+        // Brush Bottom Sheet
+        if (showBrushBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = { showBrushBottomSheet = false }
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Select Brush Size", fontSize = 18.sp)
+                    Slider(
+                        value = currentBrushSize,
+                        onValueChange = { size -> changeBrushSize(size) },
+                        valueRange = 2f..18f,
+                        steps = 16, // This allows the slider to jump by integer values
+                    )
 
-    // Brush Bottom Sheet
-    if (showBrushBottomSheet)   {
-        ModalBottomSheet(
-            onDismissRequest = { showBrushBottomSheet = false }
-        ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = "Select Brush Size", fontSize = 18.sp)
-                Slider(
-                    value = currentBrushSize,
-                    onValueChange = { size -> changeBrushSize(size) },
-                    valueRange = 2f..18f,
-                    steps = 16, // This allows the slider to jump by integer values
-                )
+                    // Brush Color Selection
+                    Text(text = "Select Brush Color", fontSize = 18.sp)
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(10.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
+                            .background(colorResource(id = R.color.lightGray))
+                            .horizontalScroll(rememberScrollState())
+                    ) {
+                        colors.forEachIndexed { index, _ ->
+                            ColorButton(
+                                buttonNumber = index,
+                                isSelected = index == currentColorIndex,
+                                colors = colors,
+                                onColorSelected = { buttonIndex ->
+                                    currentColorIndex = buttonIndex
+                                    changeBrushColor(colors[buttonIndex])
+                                }
+                            )
+                        }
+                    }
 
-                //For Brush Color Selection
-                Text(text = "Select Brush Color", fontSize = 18.sp)
-                Row(horizontalArrangement = Arrangement.spacedBy(10.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
-                        .background(colorResource(id = R.color.lightGray))
-                        .horizontalScroll(rememberScrollState())
-                ) {
-                    colors.forEachIndexed { index, _ ->
-                        ColorButton(
-                            buttonNumber = index,
-                            isSelected = index == currentColorIndex,
-                            colors = colors,
-                            onColorSelected = { buttonIndex ->
-                                currentColorIndex = buttonIndex
-                                changeBrushColor(colors[buttonIndex])
-                            }
-                        )
+                    // Background Color Selection
+                    Text(text = "Select Background Color", fontSize = 18.sp)
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(10.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
+                            .background(colorResource(id = R.color.lightGray))
+                            .horizontalScroll(rememberScrollState())
+                    ) {
+                        backgroundColors.forEachIndexed { index, _ ->
+                            ColorButton(
+                                buttonNumber = index,
+                                isSelected = index == currentBackgroundColorIndex,
+                                colors = backgroundColors,
+                                onColorSelected = { buttonIndex ->
+                                    currentBackgroundColorIndex = buttonIndex
+                                    changeBackgroundColor(backgroundColors[buttonIndex])
+                                }
+                            )
+                        }
+                    }
+
+                    // Bottom bar with buttons like Undo, Redo, Clear, Brush Size, Change Background
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        IconButton(onClick = { drawingView.value?.onClickUndo() }) {
+                            Icon(
+                                painter = painterResource(R.drawable.undo),
+                                contentDescription = "Undo",
+                                tint = Color.Unspecified,
+                                modifier = Modifier.size(32.dp)
+                            )
+                        }
+                        IconButton(onClick = { drawingView.value?.onClickRedo() }) {
+                            Icon(
+                                painter = painterResource(R.drawable.undo),
+                                contentDescription = "Redo",
+                                tint = Color.Unspecified,
+                                modifier = Modifier.size(32.dp).graphicsLayer(rotationY = 180f)
+                            )
+                        }
+                        IconButton(onClick = { drawingView.value?.clearCanvas() }) {
+                            Icon(
+                                painter = painterResource(R.drawable.clear),
+                                contentDescription = "Clear",
+                                tint = Color.Unspecified,
+                                modifier = Modifier.size(32.dp)
+                            )
+                        }
                     }
                 }
-
-                //For Background Color Selection
-                Text(text = "Select Background Color", fontSize = 18.sp)
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(10.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
-                        .background(colorResource(id = R.color.lightGray))
-                        .horizontalScroll(rememberScrollState())
-                ) {
-                    backgroundColors.forEachIndexed { index, _ ->
-                        ColorButton(
-                            buttonNumber = index,
-                            isSelected = index == currentBackgroundColorIndex,
-                            colors = backgroundColors,
-                            onColorSelected = { buttonIndex ->
-                                currentBackgroundColorIndex = buttonIndex
-                                changeBackgroundColor(backgroundColors[buttonIndex])
-                            }
-                        )
-                    }
-                }
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp), // Optional padding for extra spacing
-                    horizontalArrangement = Arrangement.SpaceEvenly // Space buttons evenly
-                ) {
-                    // IconButton for Undo
-                    IconButton(
-                        onClick = { drawingView.value?.onClickUndo() },
-                        modifier = Modifier.size(56.dp) // Set the size for the IconButton
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.undo),
-                            contentDescription = "Undo",
-                            tint = Color.Unspecified,
-                            modifier = Modifier.size(32.dp) // Increase icon size
-                        )
-                    }
-
-                    // IconButton for Redo
-                    IconButton(
-                        onClick = { drawingView.value?.onClickRedo() },
-                        modifier = Modifier.size(56.dp) // Set the size for the IconButton
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.undo),
-                            contentDescription = "Redo",
-                            tint = Color.Unspecified,
-                            modifier = Modifier
-                                .size(32.dp) // Increase icon size
-                                .graphicsLayer(rotationY = 180f) // Rotate for redo
-                        )
-                    }
-
-                    // IconButton for Clear Canvas
-                    IconButton(
-                        onClick = { drawingView.value?.clearCanvas() },
-                        modifier = Modifier.size(56.dp) // Set the size for the IconButton
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.clear),
-                            contentDescription = "Clear",
-                            tint = Color.Unspecified,
-                            modifier = Modifier.size(32.dp) // Increase icon size
-                        )
-                    }
-
-                    // IconButton for Brush Size
-                    IconButton(
-                        onClick = { }, // Function for brush size change
-                        modifier = Modifier.size(56.dp) // Set the size for the IconButton
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.share), // Replace with the correct drawable for Brush Size
-                            contentDescription = "Brush Size",
-                            tint = Color.Unspecified,
-                            modifier = Modifier.size(32.dp) // Increase icon size
-                        )
-                    }
-
-                    // IconButton for Change Background
-                    IconButton(
-                        onClick = {  }, // Function for background color change
-                        modifier = Modifier.size(56.dp) // Set the size for the IconButton
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.ai), // Replace with the correct drawable for Background Change
-                            contentDescription = "Change Background",
-                            tint = Color.Unspecified,
-                            modifier = Modifier.size(32.dp) // Increase icon size
-                        )
-                    }
-                }
-
-
             }
         }
     }
-
 }
 
 @Preview(showBackground = true, showSystemUi = true)
