079219c962b29c63fb1ca2c1690eeca66b9dbe8c
Deepon Das <deeponsubhro@gmail.com>
2025-04-11 18:31:31 +0530

    Perfect Drawing App till now

--- app/src/main/java/com/example/drawingboardiwithjc/DrawingView.kt
+++ app/src/main/java/com/example/drawingboardiwithjc/DrawingView.kt
@@ -131,41 +131,6 @@ class DrawingView(context: Context, attrs: AttributeSet?) : View(context, attrs)
                 val features = getProcessedFeatures(this) // Ensure this returns a FloatArray
                 Log.d("ProcessedFeatures", "Extracted Features: $features")
                 Log.d("ProcessedFeatures","Extracted Features List Size: ${features.size}")
-/*
-                val classifier = TFLiteClassifier(context)
-                var predictedClassIndex: Int = 0
-                try {
-                    predictedClassIndex = classifier.predict(features)
-                } catch (e: IllegalArgumentException) {
-                    Log.e("TFLiteClassifier", "Prediction failed: ${e.message}")
-                    Toast.makeText(context, "Draw a complete character and try again", Toast.LENGTH_SHORT).show()
-                }
-                val labelClasses = arrayOf(
-                    "A/अ/অ","AA/आ/আ","ADA/ढ़/ঢ","AN/ं/ং",
-                    "BA/ब/ব","BHA/भ/ভ","BI/ः/ঃ",
-                    "C/च/চ","CH/छ/ছ","CN/ँ/ঁ",
-                    "DA/द/দ","DDA/ड/ড","DDH/ढ/ঢ","DHA/ध/ধ","DRA/ड़/ড়",
-                    "E/ए/এ","EN/ञ/ঞ",
-                    "G/ग/গ","GH/घ/ঘ",
-                    "HA/ह/হ",
-                    "I/इ/ই","II/ई/ঈ",
-                    "JA/ज/জ","JH/झ/ঝ",
-                    "K/क/ক","KH/ख/খ","KT/त्/ৎ",
-                    "LA/ल/ল",
-                    "MA/म/ম","MN/ण/ণ","MSA/ष/ষ",
-                    "NA/न/ন",
-                    "O/ओ/ও","OI/ऐ/ঐ","OU/औ/ঔ",
-                    "PA/प/প","PHA/फ/ফ",
-                    "RA/र/র","RI/ऋ/ঋ",
-                    "S/स/স","SA/श/শ",
-                    "T/ट/ট","TA/त/ত","THA/थ/থ","TTA/ठ/ঠ",
-                    "U/उ/উ","UN/ङ/ঙ","UU/ऊ/ঊ",
-                    "Y/य़/য়","YA/य/য"
-                )
-                val predictedChar = labelClasses[predictedClassIndex]
-                Log.d("Prediction", "Predicted Class Index: $predictedClassIndex")
-                Log.d("Prediction", "Predicted Class Index: $predictedChar")
-                Toast.makeText(context, "Predicted Character : $predictedChar", Toast.LENGTH_LONG).show()*/
 
                 val classifier = TFLiteClassifier(context)
                 try {
